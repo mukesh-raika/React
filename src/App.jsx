ce70f176c6cf34442d28b7c2719e2d338f3452c2
mukesh <mukesh@gmail.com>
2025-12-03 18:09:15 +0530

    controlled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useState } from "react"
+import { ssrExportNameKey } from "vite/module-runner";
 
 function App() {
-  const[val,setVal]=useState("")
+  const [name,setName]=useState('');
+  const [password,setPassword]=useState('');
+  const [email,setEmail]=useState('');
   return(
-    <div>
-      <h1>get Input field value</h1>
-      <input type="text" value={val} onChange={(event)=>setVal(event.target.value)} placeholder="Enter User Name"/>
-      <h1>{val}</h1>
-      <button onClick={()=>setVal("")}>Clear value</button>
+    <div> 
+      <h1>Controller componend</h1>
+    <from action=""method="get">
+      <input type="text"value={name} onChange={(event)=>setName(event.target.value)}  placeholder="Enter Name" />
+      <br/> <br />
+       <input type="password" value={password} onChange={(event)=>setPassword(event.target.value)} placeholder="Enter password" />
+      <br/> <br />
+       <input type="text"value={email} onChange={(event)=>setEmail(event.target.value)} placeholder="Enter email" />
+      <br/> <br />
+      <button>submit</button>
+
+      <button onClick={()=>{setEmail('');setName('');setPassword('')}}>clear</button>
+
+
+
+      <h3>{name}</h3>
+      <h3>{password}</h3>
+      <h3>{email}</h3>
+    </from>
     </div>
   )
 }
-export default App;
+export default App
