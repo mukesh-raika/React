8422db224cc73567e43d3c954e646f4b9154f953
mukesh <mukesh@gmail.com>
2025-12-09 11:37:28 +0530

    style Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,103 @@
-import { useState } from "react";
+import './css/style.css'
 
 function App() {
-  const [cardStyle, setCardStyle] = useState({
-    border: "1px solid #cccccc3b",
-    width: "250px",
-    boxShadow: "1px 2px 3px 0px #cccccc57",
-    margin: "10px",
-    padding: "10px",
-    borderRadius: "10px"
-  });
-  const [textColor,setTextColor]=useState('black');
-  const[grid,setGrid]=useState('true') 
-
-  const updateTheme = (bgColor, textColor) => {
-    setCardStyle({ ...cardStyle, backgroundColor: bgColor})
-      setTextColor(textColor)
-    }
-
   return (
     <>
-      <h1 style={{ color: "red" }}>Dynamic and Condition Inline Style</h1>
-
-      <button onClick={() => updateTheme("gray", "green")}>Gray Theme</button>
-      <button onClick={() => updateTheme("white", "black")}>Default Theme</button>
-      <button onClick={()=>setGrid(!grid)}>Toggle Grid</button>
-      
-      <div style={{ display: grid?'flex':'block', flexWrap: 'wrap' }}>
-        
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt2zvpO6R6ZgGxB-qRp6NxexAZjkH5MndFb-lezV4uOg&s"
-            alt="demo"
-          />
-          <h4>Ninja Coder</h4>
-          <p>Software Developer</p>
+      <h1 className="heading">External Style</h1>
+      <div className='container'>
+      <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
         </div>
-
-        
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt2zvpO6R6ZgGxB-qRp6NxexAZjkH5MndFb-lezV4uOg&s"
-            alt="demo"
-          />
-          <h4>React Developer</h4>
-          <p>Frontend Engineer</p>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+      <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
         </div>
-
-        
-        <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt2zvpO6R6ZgGxB-qRp6NxexAZjkH5MndFb-lezV4uOg&s"
-            alt="demo"
-          />
-          <h4>Full-Stack Ninja</h4>
-          <p>MERN Developer</p>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+     <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
         </div>
-
-  <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt2zvpO6R6ZgGxB-qRp6NxexAZjkH5MndFb-lezV4uOg&s"
-            alt="demo"
-          />
-          <h4>Full-Stack Ninja</h4>
-          <p>MERN Developer</p>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+      <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
         </div>
-
-  <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt2zvpO6R6ZgGxB-qRp6NxexAZjkH5MndFb-lezV4uOg&s"
-            alt="demo"
-          />
-          <h4>Full-Stack Ninja</h4>
-          <p>MERN Developer</p>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+      <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
         </div>
-
-  <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt2zvpO6R6ZgGxB-qRp6NxexAZjkH5MndFb-lezV4uOg&s"
-            alt="demo"
-          />
-          <h4>Full-Stack Ninja</h4>
-          <p>MERN Developer</p>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+      <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
         </div>
-
-  <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt2zvpO6R6ZgGxB-qRp6NxexAZjkH5MndFb-lezV4uOg&s"
-            alt="demo"
-          />
-          <h4>Full-Stack Ninja</h4>
-          <p>MERN Developer</p>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+      <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
         </div>
-
-  <div style={cardStyle}>
-          <img
-            style={{ width: "200px" }}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt2zvpO6R6ZgGxB-qRp6NxexAZjkH5MndFb-lezV4uOg&s"
-            alt="demo"
-          />
-          <h4>Full-Stack Ninja</h4>
-          <p>MERN Developer</p>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+      <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
         </div>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+      <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
+        </div>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+      <div className='user-card'> 
+        <div>
+          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
+        </div>
+        <div className=' text-wrap'>
+        <h4>Anil Sidhu</h4>
+        <p>Software developer</p>
+      </div>
+      </div>
+
+
+
 
       </div>
     </>
