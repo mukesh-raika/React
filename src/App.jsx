9da34626cc4f4d97c19b15a325cc6f50ebae7c3f
mukesh <mukesh@gmail.com>
2025-12-09 12:28:09 +0530

    module Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,23 @@
-import './css/style.css'
+import UserProfile from "./Userprofile";
 
 function App() {
   return (
     <>
-      <h1 className="heading">External Style</h1>
-      <div className='container'>
-      <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-      <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-     <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-      <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-      <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-      <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-      <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-      <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-      <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-      <div className='user-card'> 
-        <div>
-          <img className='img-style'src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQegKbRhqMMus5P1yfqdwmH9_vsHUFIaKqt7ZeTmYc7s-2fSlRUytsIjeZpF_de9Bvl8qI&usqp=CAU" />
-        </div>
-        <div className=' text-wrap'>
-        <h4>Anil Sidhu</h4>
-        <p>Software developer</p>
-      </div>
-      </div>
-
-
+      <h1>Style with Css module in React js</h1>
+      <div style={{display:"flex",flexWrap:"warp"}}>
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+     <UserProfile />  
+    <UserProfile />
+     <UserProfile />
+      <UserProfile />
+     <UserProfile />
+    <UserProfile />
+     <UserProfile />
 
 
       </div>
