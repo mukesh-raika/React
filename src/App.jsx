2bfc75171ac5ee20b4e5586ea95c982acf351e84
mukesh <mukesh@gmail.com>
2025-12-09 13:11:47 +0530

    style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
-import UserProfile from "./Userprofile";
+import styled from "styled-components"
 
-function App() {
-  return (
+function App(){
+  
+  // const Heading =styled.h1`
+  // color:red;
+  // border:1px solid green;
+  //  border-radius:5px;
+  //  margin:20px;
+  //  padding:20px
+  // `
+
+  const Heading=styled.h1({
+  color:'red',
+  border:'1px solid green',
+   borderRadius:'5px',
+   margin:'20px',
+   padding:'20px',
+  })
+
+  const StyleBtn=styled.button`
+  color:red; 
+  width:130px;
+  height:40px ;
+  margin:20px;
+
+  `
+
+  return(
     <>
-      <h1>Style with Css module in React js</h1>
-      <div style={{display:"flex",flexWrap:"warp"}}>
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
-     <UserProfile />  
-    <UserProfile />
-     <UserProfile />
-      <UserProfile />
-     <UserProfile />
-    <UserProfile />
-     <UserProfile />
-
-
-      </div>
+      <h1>Style component with React js</h1>
+      <Heading>Hello heading1</Heading>
+      <Heading>Hello heading2</Heading>
+      <Heading>Hello heading3</Heading>
+      <Heading>Hello heading4</Heading>
+      <Heading>Hello heading5</Heading>
+     <StyleBtn>Login</StyleBtn>
+     <StyleBtn>signup</StyleBtn>
+     <StyleBtn>Bakc </StyleBtn>
+     <StyleBtn>logaut</StyleBtn>
     </>
-  );
+  )
 }
-
-export default App;
+export default App
--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -2,7 +2,6 @@ import style from './css/UserProfile.module.css'
 function UserProfile() {
   return (
     <div>
-            
       <div className={style.card}>
         <img className={style.img}  src="https://img.freepik.com/premium-vector/laptop-coding-concept-web-developer-programming-laptop-screen-code-illustration_100456-1752.jpg"alt="Developer"/>
         <div className={style.textWrap}>
