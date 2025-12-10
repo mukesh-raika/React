94fc342f2a71fa8525c67db8b1e5b41a7ce67874
mukesh <mukesh@gmail.com>
2025-12-10 11:30:19 +0530

    form status

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import { useRef } from "react";
-import UserInput from "./Userinput";
+import { useFormStatus } from "react-dom";
 
-function App(){
-  const inputRef=useRef(null)
+function App() {
 
-  const updateInput=()=>{
-    inputRef.current.value=1000;
-    inputRef.current.focus();
-   inputRef.current.style.color="red" 
+  async function handleSubmit(formData) {
+    
+    await new Promise((res) => setTimeout(res, 2000));
+    console.log("submit");
   }
-  return(
-    <>
-    <h1>Forward Ref</h1>
-    <UserInput ref={inputRef} />
-    <button onClick={updateInput}>Update Input field</button>
-    </>
-  )
+
+  function CustomerForm() {
+    const { pending } = useFormStatus();
+    console.log(pending);
+
+    return (
+      <>
+        <input type="text" name="username" placeholder="Enter Name" />
+        <br />
+        <br />
+        <input type="password" name="password" placeholder="Enter Password" />
+        <br />
+        <br /> 
+        <button disabled={pending}>{pending ? "Submitting..." : "Submit"}</button>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <h1>useFormStatus Hook in React 19</h1>
+
+      <form action={handleSubmit}>
+        <CustomerForm />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
