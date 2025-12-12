4f9dcddcac1d0910e8cee40618b3b076ca966bc4
mukesh <mukesh@gmail.com>
2025-12-12 16:16:52 +0530

    Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,21 @@
-import { useId } from "react"
 
-export default function App() {
+
+export default function App(){
+  return (
+    <>
+    <Data/>
+    <Data/>
+    <Data/>
+    </>
+    
   
-  return(
-   <div>
-    <UserForm/>
-    <hr/>
-     <UserForm/>
-   </div>
   )
-
 }
-
- function UserForm() {
-  const user =useId();
- 
+function Data(){
   return(
     <div>
-     <form action="">
-      <label htmlFor={user+"name"}>Enter User Name</label>
-      <input id={user+"name"} type="text" placeholder="enter name" />
-      <br/>
-      <br/>      
-      <label htmlFor={user+"password"}>Enter User Password</label>
-      <input id={user+"password"}type="text" placeholder="enter password" />
-      <br/>
-      <br/>
-      <label htmlFor={user+"skills"}>Enter User Skills</label>
-      <input id={user+"skills"}type="text"placeholder="enter skills"/>
-      <br/>
-      <br/>
-      <input id={user+"terms"}type="checkbox" placeholder="enter skills"/>
-      <label htmlFor={user+"terms"}>Terms and Condition</label>
-     </form>
+    <span>Hello user</span>
+       
     </div>
   )
-
 }
-
-
